b8f1f5229f26b7cc22fc1ff1ccdbdd20b95f8c12
luckyshanb4 <luckyshanb4@gmail.com>
2021-07-01 10:54:11 +0530

    add component using angular CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CribListingComponent } from './crib-listing/crib-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CribListingComponent
   ],
   imports: [
     BrowserModule,
